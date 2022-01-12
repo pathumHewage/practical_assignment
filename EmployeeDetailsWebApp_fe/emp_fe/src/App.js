8fb77afe03c73bc98500115d18ed2a5f616c17e5
pathumHewage <it20185220@my.sliit.lk>
2022-01-12 17:51:32 +0530

    update api and create fe

---
+++ EmployeeDetailsWebApp_fe/emp_fe/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Home} from './components/Home';
+import {Employee} from './components/Employee';
+import {BrowserRouter, Route, Switch,NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <h3 className='d-flex justify-content-center m-3'>
+       React Forntend
+     </h3>
+    </div>
+  );
+}
+
+export default App;
