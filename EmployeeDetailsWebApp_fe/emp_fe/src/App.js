bf2c403e9171846dce187b28378c2ea2c9ea0c95
pathumHewage <it20185220@my.sliit.lk>
2022-01-12 20:56:51 +0530

    create department fe and CRUD

--- EmployeeDetailsWebApp_fe/emp_fe/src/App.js
+++ EmployeeDetailsWebApp_fe/emp_fe/src/App.js
@@ -1,16 +1,53 @@
 import './App.css';
 import {Home} from './components/Home';
 import {Employee} from './components/Employee';
-import {BrowserRouter, Route, Switch,NavLink} from 'react-router-dom';
+import { Department } from './components/Department';
+import { BrowserRouter,Route,Routes,NavLink} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <h3 className='d-flex justify-content-center m-3'>
-       React Forntend
-     </h3>
-    </div>
-  );
-}
+    <BrowserRouter>
 
-export default App;
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-3">
+        React JS Frontend
+      </h3>
+        
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/department">
+              Department
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+              Employee
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+ 
+
+    <Routes>
+
+    <Route path="/home" element={<Home/>} />
+    <Route path="/employee" element={<Employee/>} />
+    <Route path="/department" element={<Department/>} />
+   
+
+  </Routes>
+</div>
+</BrowserRouter>
+  );
+ }
+
+ export default App;
+
+
